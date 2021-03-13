70f9c03f9e6d5ea32d809806918c58375b5ebcca
Ryan Beiden <rdbeiden@gmail.com>
2021-03-13 12:32:14 -0600

    remove secret key and replace with a new key through virtualenv

--- cmdfood/settings.py
+++ cmdfood/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%623_y=q&_gqq4m(-ff7=$krsm7pucki@*f$w9bk8v4vpfxnc%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
