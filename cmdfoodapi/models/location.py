0dd40b9b867a027b2bcc8490c57b957e7cc757fb
Ryan Beiden <rdbeiden@gmail.com>
2021-03-16 21:15:13 -0500

    update character length for location name

--- cmdfoodapi/models/location.py
+++ cmdfoodapi/models/location.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Location(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=50)
     city = models.CharField(max_length=25)
     state = models.CharField(max_length=25)
     zip_code = models.PositiveIntegerField(validators=[MinValueValidator(9999), MaxValueValidator(100000)])
