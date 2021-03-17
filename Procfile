81d35b6ce8f4f085370ea98af93a150803b390b6
Ryan Beiden <rdbeiden@gmail.com>
2021-03-16 20:00:58 -0500

    installs for intial heroku setip

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cmdfood.wsgi --log-file -
--- cmdfood/settings.py
+++ cmdfood/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,7 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+django_on_heroku.settings(locals())
 
 # Local Media URL (only when Debug is set to True)
 
