1b64d79285fbe67a647ece3e80a3db5df6e064ca
Ryan Beiden <rdbeiden@gmail.com>
2021-03-22 18:54:47 -0500

    update product model to remove aisle info except aisle description

--- cmdfoodapi/models/product.py
+++ cmdfoodapi/models/product.py
@@ -6,6 +6,4 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=19, decimal_places=2)
     image_url = models.CharField(max_length=500)
-    aisle_number = models.IntegerField()
-    aisle_side = models.CharField(max_length=2)
-    aisle_bay = models.CharField(max_length=10)
+    aisle = models.CharField(max_length=100)
