e06aa704bdc2c740d8b70821f1644dd88ddae59c
Ryan Beiden <rdbeiden@gmail.com>
2021-03-21 20:18:12 -0500

    update image field for product model

--- cmdfoodapi/models/product.py
+++ cmdfoodapi/models/product.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     kroger_id = models.CharField(max_length=25)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=19, decimal_places=4)
-    image_url = models.ImageField(_("Product Image"), blank=True, upload_to='products/')
+    image_url = models.CharField(max_length=500)
     aisle_number = models.IntegerField()
     aisle_side = models.CharField(max_length=2)
     aisle_bay = models.CharField(max_length=10)
