caedfe0e37ba418d0ef013fe8ab1b9cd760a8611
Ryan Beiden <rdbeiden@gmail.com>
2021-03-21 20:27:53 -0500

    update price decimal count on product model

--- cmdfoodapi/models/product.py
+++ cmdfoodapi/models/product.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 class Product(models.Model):
     kroger_id = models.CharField(max_length=25)
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=19, decimal_places=4)
+    price = models.DecimalField(max_digits=19, decimal_places=2)
     image_url = models.CharField(max_length=500)
     aisle_number = models.IntegerField()
     aisle_side = models.CharField(max_length=2)
